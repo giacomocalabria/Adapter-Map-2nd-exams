12e2a02fb6ea025c2845cbb3ef60bf8ac4c95033
Giacomo Calabria <giacomo.calabria.01@gmail.com>
2022-07-03 16:22:40 +0200

    doc

--- myAdapter/HMap.java
+++ myAdapter/HMap.java
@@ -253,13 +253,63 @@ public interface HMap{
      */
 
     public interface HEntry {
+
+        /**
+         *  Compares the specified object with this entry for equality. Returns true if the
+         *  given object is also a map entry and the two entries represent the same mapping.
+         *  More formally, two entries e1 and e2 represent the same mapping if
+         * 
+         *  (e1.getKey()==null ? e2.getKey()==null : e1.getKey().equals(e2.getKey())) &&
+         *  (e1.getValue()==null ? e2.getValue()==null : e1.getValue().equals(e2.getValue()))
+         * 
+         *  <p>
+         *  This ensures that the equals method works properly across different
+         *  implementations of the Map.Entry interface.
+         *  @param o true if the specified object is equal to this map entry.
+         *  @return true if the specified object is equal to this map entry.
+         */
+
         public boolean equals(Object o);
 
+        /**
+         *  Returns the key corresponding to this entry.
+         * 
+         *  @return the key corresponding to this entry.
+         */
         public Object getKey();
+
+        /**
+         *  Returns the value corresponding to this entry. If the mapping has been
+         *  removed from the backing map (by the iterator's remove operation),
+         *  the results of this call are undefined.
+         * 
+         *  @return the value corresponding to this entry.
+         */
 
         public Object getValue();
 
+        /**
+         *  Returns the hash code value for this map entry. The hash code of a map
+         *  entry e is defined to be:
+         *  (e.getKey()==null   ? 0 : e.getKey().hashCode()) ^ (e.getValue()==null ? 0 : e.getValue().hashCode())
+         * 
+         *  <p>
+         *  This ensures that e1.equals(e2) implies that e1.hashCode()==e2.hashCode()
+         *  for any two Entries e1 and e2, as required by the general contract of Object.hashCode.
+         *  @return the hash code value for this map entry.
+         */
+
         public int hashCode();
+
+        /**
+         *  Replaces the value corresponding to this entry with the specified value
+         *  (optional operation). (Writes through to the map.) The behavior of this
+         *  call is undefined if the mapping has already been removed from
+         *  the map (by the iterator's remove operation).
+         *  
+         *  @param value new value to be stored in this entry.
+         *  @return old value corresponding to the entry.
+         */
 
         public Object setValue(Object value);
     }
